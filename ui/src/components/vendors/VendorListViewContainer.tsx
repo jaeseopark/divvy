f4848045a8ba7a5754f9c81978af5c56a2bbddbb
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2026-07-21 21:23:02 -0600

    fix: vendor detail onclick handler (#66)

--- ui/src/components/vendors/VendorListViewContainer.tsx
+++ ui/src/components/vendors/VendorListViewContainer.tsx
@@ -2,13 +2,20 @@ import { useNavigate } from "react-router-dom";
 
 import { ROUTE_PATHS } from "@/const";
 import { sigVendors } from "@/store";
+import { getEditVendorPath } from "@/utils/paths";
 
 import VendorListView from "@/components/vendors/VendorListView";
 
 const VendorListViewContainer = () => {
   const navigate = useNavigate();
 
-  return <VendorListView vendors={sigVendors.value} onAdd={() => navigate(ROUTE_PATHS.NEW_VENDOR)} />;
+  return (
+    <VendorListView
+      vendors={sigVendors.value}
+      onAdd={() => navigate(ROUTE_PATHS.NEW_VENDOR)}
+      onClick={(vendor) => navigate(getEditVendorPath(vendor.id))}
+    />
+  );
 };
 
 export default VendorListViewContainer;
